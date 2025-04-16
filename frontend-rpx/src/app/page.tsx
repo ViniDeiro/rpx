b91d4193d03c014fb10443b8315b575da607e413
Seu Nome <seu.email@exemplo.com>
2025-04-16 04:13:44 -0300

    Removido ícone Trophy não existente na biblioteca react-feather

--- frontend-rpx/src/app/page.tsx
+++ frontend-rpx/src/app/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { ArrowRight, Check, Flag, Star, Trophy, Users } from 'react-feather';
+import { ArrowRight, Check, Flag, Star, Users } from 'react-feather';
 import { formatCurrency } from '@/utils/formatters';
 import { SponsorPromoBanner } from '@/components/ui/SponsorPromoBanner';
 
